bb1ba37fb5c2a3a3634950ed28b0c20550b3f673
JoanMburu <joan.wangui@student.moringaschool.com>
2024-05-21 19:00:55 +0300

    update hooks state and events

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  // State variable to track if the item is in the cart
+  const [isInCart, setIsInCart] = useState(false);
+
+  // Toggle function for adding/removing item from cart
+  const handleCartToggle = () => {
+    setIsInCart(!isInCart);
+  };
+
+  const itemClass = isInCart ? "in-cart" : "";
+
   return (
-    <li className="">
+    <li className={itemClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick={handleCartToggle}>
+        {isInCart ? "Remove From Cart" : "Add to Cart"}
+      </button>
     </li>
   );
 }
